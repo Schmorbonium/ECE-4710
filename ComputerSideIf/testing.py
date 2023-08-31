fc53d525f2a34e91b628461eaa20ece30e349432
dknelson9876 <dknelson9876@gmail.com>
2023-08-31 12:52:42 -0600

    minor fixes

--- ComputerSideIf/testing.py
+++ ComputerSideIf/testing.py
@@ -1,5 +1,6 @@
 # pip install pyserial
 import serial
+import serial.tools.list_ports
 import time
 
 def findActiveSerialPort():
@@ -29,7 +30,7 @@ def getHeader(addr:list[int],ttl:int,data_len:int,id:int) -> list[int]:
 
     hdr2:int = 0
     hdr2 |= (data_len & 0x7) << 5 
-    hdr2 |= (id & 0xF)
+    hdr2 |= (id & 0x1F)
 
     return [hdr1,hdr2]
 
@@ -51,23 +52,28 @@ def toggleGMsg(addr:list[int]) -> list[int]:
 
 def sendMsg(msg:list[int],ser:serial.SerialBase):
     for i in msg :
-        ser.write(i & 0xff)
+        
+        ser.write(bytes(i & 0xff))
 
 
 activePort = findActiveSerialPort()
 with serial.Serial(activePort, 115200, timeout=1) as ser:
     while True:
-        msg:list[int] = toggleBMsg([1])
+        msg:list[int] = toggleBMsg([0,1,2,3])
         sendMsg(msg,ser)
+        print(f'Sent {msg[0]:02X}{msg[1]:02X}')
         time.sleep(1)
         msg:list[int] = toggleGMsg([2])
         sendMsg(msg,ser)
+        print(f'Sent {msg}')
         time.sleep(1)
         msg:list[int] = toggleOMsg([1,2])
         sendMsg(msg,ser)
+        print(f'Sent {msg}')
         time.sleep(1)
         msg:list[int] = toggleRMsg([2])
         sendMsg(msg,ser)
+        print(f'Sent {msg}')
         time.sleep(1)
 
 
