c1eb39c425fe93a8ac8250154e965fd5e3d98544
izeek182 <izeek182@gmail.com>
2023-09-14 16:02:59 -0600

    interface V2

--- memIo/Core/Inc/comIf.h
+++ memIo/Core/Inc/comIf.h
@@ -7,7 +7,15 @@
 extern "C" {
 #endif
 
+/// @brief Initializes The computer interface on the given UART connection
+/// @param Core Pointer to the HAL Uart Handle
+void CompIfInit(UART_HandleTypeDef* Core);
 
+/// @brief This method handles the Uart interrupt for this interface 
+void CompIfUartInterruptHAndler();
+
+/// @brief This Method handles all of the computer interface's work that can be completed synchonously.
+void CompIfSyncHandler();
 
 #ifdef __cplusplus
 }
