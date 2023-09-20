c383c216e101a31a54f21dd67ec1a960112392ba
izeek182 <izeek182@gmail.com>
2023-09-19 23:46:25 -0600

    testing shift register

--- memIo/Core/Src/main.cpp
+++ memIo/Core/Src/main.cpp
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -27,6 +27,7 @@
 /* USER CODE BEGIN Includes */
 #include "gpioObj.hpp"
 #include "bufferedUart.hpp"
+#include "shiftReg.hpp"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -65,9 +66,9 @@ BufferedUart bufUart2(&huart2);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -100,22 +101,56 @@ int main(void)
   /* Initialize interrupts */
   MX_NVIC_Init();
   /* USER CODE BEGIN 2 */
-  GPIO statusLED(LED_PIN_GPIO_Port,LED_PIN_Pin);
-  bufUart2.startListening();
-  char string[] = "hello World!";
-  bufUart2.send((uint8_t*)&string,12);
+  // GPIO statusLED(LED_PIN_GPIO_Port,LED_PIN_Pin);
+  // bufUart2.startListening();
+  // char string[] = "hello World!";
+  // bufUart2.send((uint8_t*)&string,12);
+
+  ShiftReg shifter((uint16_t)3, LED_CLK_GPIO_Port, LED_CLK_Pin, LED_LATCH_GPIO_Port, LED_LATCH_Pin, LED_DATA_GPIO_Port, LED_DATA_Pin);
+
+  shifter.setChipMask(0, 0x0000);
+  shifter.setChipMask(1, 0x0000);
+  shifter.setChipMask(2, 0x0000);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int index = 0;
   while (1)
   {
-    /* USER CODE END WHILE */
-    if(bufUart2.getInputSize() >= 8){
-      uint8_t buffer[8];
-      bufUart2.takeFromInbox(buffer,8);
-      bufUart2.send(buffer,8);
+    __disable_irq();
+    shifter.asyncUpdate();
+    __enable_irq();
+    // HAL_Delay(1);
+    for (volatile size_t i = 0; i < 1000; i++)
+    {
+      /* code */
     }
+    
+    if (!shifter.busy())
+    {
+      if (index >= 16)
+      {
+        index = 0;
+      }
+      if (index == 0)
+      {
+        shifter.resetBit(0, 15);
+        shifter.resetBit(1, 15);
+        shifter.resetBit(2, 15);
+      }
+      else
+      {
+        shifter.resetBit(0, index - 1);
+        shifter.resetBit(1, index - 1);
+        shifter.resetBit(2, index - 1);
+      }
+      shifter.setBit(0, index);
+      shifter.setBit(1, index);
+      shifter.setBit(2, index);
+      index++;
+    }
+    /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
@@ -123,17 +158,17 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -144,9 +179,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -159,9 +193,9 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief NVIC Configuration.
-  * @retval None
-  */
+ * @brief NVIC Configuration.
+ * @retval None
+ */
 static void MX_NVIC_Init(void)
 {
   /* USART1_IRQn interrupt configuration */
@@ -177,9 +211,9 @@ static void MX_NVIC_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -191,14 +225,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
