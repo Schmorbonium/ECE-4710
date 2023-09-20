0132395c264425b23cb838319d85f4ee6395dbaa
izeek182 <izeek182@gmail.com>
2023-09-19 23:46:25 -0600

    testing shift register

--- memIo/Core/Src/gpio.c
+++ memIo/Core/Src/gpio.c
@@ -66,7 +66,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pins : PAPin PAPin PAPin */
   GPIO_InitStruct.Pin = LED_DATA_Pin|LED_CLK_Pin|LED_LATCH_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
